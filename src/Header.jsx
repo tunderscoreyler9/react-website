73d050eb38a5850930e5aeb3a5f8d35534542be4
Tyler <tyler.s9@proton.me>
2025-01-26 21:59:47 -0500

    First change to announce this is the portfolio site

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@
 function Header() {
     return (
         <header>
-            <h1>My Website</h1>
+            <h1>My Portfolio</h1>
             <nav>
                 <ul>
                     <li><a href="#">Home</a></li>
