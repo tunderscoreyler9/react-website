f215efc0ceffbeaffe0b86e4c94fcece73aa7ea0
Tyler <tyler.s9@proton.me>
2025-02-01 19:47:29 -0500

    Continuing along lol

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import logo from './images/osfi2ccuar131.jpg'
+
+function Card() {
+   return (
+        <div className="card">
+            <img src={logo} alt="Profile picture" />
+            <h2>Tyler S</h2>
+            <p>I am learning ReactJS</p>
+            
+        </div>
+   ); 
+};
+
+export default Card;
