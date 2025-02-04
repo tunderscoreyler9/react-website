64b715067224127adc278ba965bced4cbc90a2b7
Tyler <tyler.s9@proton.me>
2025-02-03 19:03:28 -0500

    Fixed image card height and width

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import logo from './images/osfi2ccuar131.jpg'
 function Card() {
    return (
         <div className="card">
-            <img src={logo} alt="Profile picture" />
+            <img src={logo} alt="Profile picture" width='200px' height='200px' />
             <h2>Tyler S</h2>
             <p>I am learning ReactJS</p>
             
