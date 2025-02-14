f73b9872ef45446eb44be1e74f069f40cfce8f18
Tyler <tyler.s9@proton.me>
2025-02-14 12:44:31 -0500

    Added some notes.

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,6 +3,8 @@
 function Button() {
 
     const styles = {
+        // JSX code (below) requires camel case naming conventions for CSS
+        // All values should strings with commas to separate (just like an object's syntax)
         backgroundColor: "hsl(200, 100%, 50%)",
         color: "white",
         padding: "10px 20px",
